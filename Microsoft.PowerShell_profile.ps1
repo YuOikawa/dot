896de9fd5477754c7d190e1e04dc6aa3adc7dbb3
yoikawa <yoikawa@valuecommerce.co.jp>
2018-07-26 10:52:49 +0900

    powershell
    powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+Set-Alias vim 'C:\Program Files\vim80\vim.exe'
+Set-PSReadlineKeyHandler -Key 'Ctrl+u' -Function BackwardDeleteLine
+Set-PSReadlineKeyHandler -Key 'Ctrl+b' -Function BackwardChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+f' -Function ForwardChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+d' -Function DeleteChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+h' -Function BackwardDeleteChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key 'Ctrl+a' -Function BeginningOfLine
+Set-PSReadlineKeyHandler -Key 'Ctrl+e' -Function EndOfLine
+(Get-Host).UI.RawUI.WindowTitle = "PowerShell Yeah!!"
